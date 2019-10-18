cab07b14f23b85b03a3b859d0e43e7f7af5b366e
pfesenmeier@gmail.com <pfesenmeier@gmail.com>
2019-10-18 09:28:37 -0400

    Working Website

--- Larder/Global.asax.cs
+++ Larder/Global.asax.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace Larder
+namespace Larder.WebMVC
 {
     public class MvcApplication : System.Web.HttpApplication
     {

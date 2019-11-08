d454fa08643bde690b6bed3fa82b0f41d13cef57
pfesenmeier@gmail.com <pfesenmeier@gmail.com>
2019-11-08 10:15:57 -0500

    LarderController routes

--- Larder.WebAPI/Controllers/LarderController.cs
+++ Larder.WebAPI/Controllers/LarderController.cs
@@ -1,4 +1,5 @@
-﻿using Larder.Services;
+﻿using Larder.Models;
+using Larder.Services;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -12,11 +13,46 @@ namespace Larder.WebAPI.Controllers
     [Authorize]
     public class LarderController : ApiController
     {
-        public IHttpActionResult Get()
+        public IHttpActionResult GetAll()
         {
             LarderService larderService = CreateLarderService();
             var larders = larderService.GetLarders();
             return Ok(larders);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            LarderService larderService = CreateLarderService();
+            var larder = larderService.GetLarderbyId(id);
+            return Ok(larder);
+        }
+
+        public IHttpActionResult Post(LarderCreate larder)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateLarderService();
+            service.CreateLarder(larder);
+            return Ok();
+        }
+
+        public IHttpActionResult Put(LarderEdit larder)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateLarderService();
+            if (!service.UpdateLarder(larder))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateLarderService();
+            if (!service.DeleteLarder(id))
+                return InternalServerError();
+            return Ok();
         }
 
         private LarderService CreateLarderService()
